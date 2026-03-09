9947550bf8cec6296da5d7a321fcb73f89f9925f
Rishit Thakur <rishitthakur147@gmail.com>
2026-03-09 17:16:47 +0530

    fix: hardcode vercel URL in CORS

--- server/server.js
+++ server/server.js
@@ -16,7 +16,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:4173',
-  process.env.FRONTEND_URL
+  process.env.FRONTEND_URL,
+  'https://sharetrack-74206fq8c-rishit-thakur7s-projects.vercel.app'
 ].filter(Boolean);
 
 app.use(cors({ origin: allowedOrigins, credentials: true }));
